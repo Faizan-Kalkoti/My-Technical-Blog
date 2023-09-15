caaad000f50cece9bac87b68c9eaebb8d113ea86
Faizan <faizan.kalkoti@gmail.com>
2023-09-15 22:05:59 +0530

    try 4

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -25,3 +25,5 @@ urlpatterns = [
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('accounts/logout/', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns+= static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
